8a8efac00c36694ef9107ab3a054323b0c43c797
KenWang <w910014@gmail.com>
2022-11-22 12:40:19 +0800

    Modify the api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     id("com.google.firebase.firebase-perf")
     id("com.google.firebase.crashlytics")
     id("com.google.devtools.ksp")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     kotlin("kapt")
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("com.android.application") version("7.3.1") apply(false)
     id("com.android.library") version("7.3.1") apply(false)
     id("org.jetbrains.kotlin.android") version("1.7.20") apply(false)
+
+    // https://developers.google.com/maps/documentation/places/android-sdk/secrets-gradle-plugin
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version("2.0.1") apply(false)
 }
 
 buildscript {
